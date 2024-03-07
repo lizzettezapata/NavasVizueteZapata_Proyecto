496d0772219e1daa6675f289ef1710d603d0ac09
RenatoNavas <84428509+RenatoNavas@users.noreply.github.com>
2024-03-06 23:35:05 -0500

    Delete microservicios-biblioteca directory

--- microservicios-biblioteca/src/main/java/com/app/biblioteca/models/repository/AutorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.app.biblioteca.models.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.app.biblioteca.models.entity.Autor;
-
-public interface AutorRepository extends PagingAndSortingRepository<Autor, Long> {
-}
--- microservicios-biblioteca/src/main/java/com/app/biblioteca/models/repository/EstudianteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.biblioteca.models.repository;
-
-import com.app.biblioteca.models.entity.Estudiante;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EstudianteRepository extends CrudRepository<Estudiante, Long> {
-}
